870d6564ae0ed4e348c67c43018790c1d678ef20
Alexandre Macabies <web+git@zopieux.com>
2017-08-25 20:55:57 +0200

    Add optional token markers on assemble_sentence

--- cadavre/data.py
+++ cadavre/data.py
@@ -1,3 +1,5 @@
+import string
+
 PIECES = {
     'S': "sujet",
     'Se': "attribut du sujet",
@@ -36,76 +38,81 @@ LIGATURES = {
 }
 
 
-def assemble_sentence(parts, colors=True):
+def assemble_sentence(parts, mark_begin='', mark_end=''):
     """
     Assemble parts and try to keep it French.
 
-    >>> assemble_sentence(['meuf à', 'le voisin'])
-    'Meuf au voisin.'
-    >>> assemble_sentence(['meuf à', 'la voisine'])
-    'Meuf à la voisine.'
-    >>> assemble_sentence(['meuf à', 'les voisins'])
-    'Meuf aux voisins.'
-    >>> assemble_sentence(['meuf à', 'mes voisins'])
-    'Meuf à mes voisins.'
-    >>> assemble_sentence(['meuf de', 'le voisin'])
-    'Meuf du voisin.'
-    >>> assemble_sentence(['meuf de', 'la voisine'])
-    'Meuf de la voisine.'
-    >>> assemble_sentence(['meuf de', 'les voisins'])
-    'Meuf des voisins.'
-    >>> assemble_sentence(['meuf de', 'mon fils'])
-    'Meuf de mon fils.'
-    >>> assemble_sentence(['meuf de', 'un voisin'])
-    "Meuf d'un voisin."
-    >>> assemble_sentence(['meuf de', 'une nana'])
-    "Meuf d'une nana."
-    >>> assemble_sentence(['meuf de', 'des voisins'])
-    'Meuf des voisins.'
-    >>> assemble_sentence(['meuf que', ', en bon prince, il nique'])
-    'Meuf que, en bon prince, il nique.'
-    >>> assemble_sentence(['meuf que, moi,', ', en bon prince, je nique'])
-    'Meuf que, moi, en bon prince, je nique.'
-    >>> assemble_sentence(['meuf que', 'il nique'])
-    "Meuf qu'il nique."
-    >>> assemble_sentence(['meuf QUE', 'il nique'])
-    "Meuf QU'il nique."
-    >>> assemble_sentence(['meuf que', 'elle nique'])
-    "Meuf qu'elle nique."
-    >>> assemble_sentence(['meuf que', 'on baise'])
-    "Meuf qu'on baise."
-    >>> assemble_sentence(['meuf que', 'Aristote démonte'])
-    "Meuf qu'Aristote démonte."
-    >>> assemble_sentence(['meuf que', 'Ursule encule'])
-    "Meuf qu'Ursule encule."
+    >>> assemble_sentence(['meuf à', 'le voisin'], '[', ']')
+    '[Meuf] [au voisin].'
+    >>> assemble_sentence(['meuf à', 'la voisine'], '[', ']')
+    '[Meuf à] [la voisine].'
+    >>> assemble_sentence(['meuf à', 'les voisins'], '[', ']')
+    '[Meuf] [aux voisins].'
+    >>> assemble_sentence(['meuf à', 'mes voisins'], '[', ']')
+    '[Meuf à] [mes voisins].'
+    >>> assemble_sentence(['meuf de', 'le voisin'], '[', ']')
+    '[Meuf] [du voisin].'
+    >>> assemble_sentence(['meuf de', 'la voisine'], '[', ']')
+    '[Meuf de] [la voisine].'
+    >>> assemble_sentence(['meuf de', 'les voisins'], '[', ']')
+    '[Meuf] [des voisins].'
+    >>> assemble_sentence(['meuf de', 'mon fils'], '[', ']')
+    '[Meuf de] [mon fils].'
+    >>> assemble_sentence(['meuf de', 'un voisin'], '[', ']')
+    "[Meuf] [d'un voisin]."
+    >>> assemble_sentence(['meuf de', 'une nana'], '[', ']')
+    "[Meuf] [d'une nana]."
+    >>> assemble_sentence(['meuf de', 'des voisins'], '[', ']')
+    '[Meuf] [des voisins].'
+    >>> assemble_sentence(['meuf que', ', serein, il nique'], '[', ']')
+    '[Meuf que], [serein, il nique].'
+    >>> assemble_sentence(['meuf que,,', ', serein, il nique '], '[', ']')
+    '[Meuf que], [serein, il nique].'
+    >>> assemble_sentence(['meuf que', 'il nique'], '[', ']')
+    "[Meuf] [qu'il nique]."
+    >>> assemble_sentence(['meuf QUE', 'il nique', 'après'], '[', ']')
+    "[Meuf] [qu'il nique] [après]."
+    >>> assemble_sentence(['meuf QUE', 'il nie que', 'elle baise'], '[', ']')
+    "[Meuf] [qu'il nie] [qu'elle baise]."
+    >>> assemble_sentence(['meuf que', 'elle nique'], '[', ']')
+    "[Meuf] [qu'elle nique]."
+    >>> assemble_sentence(['meuf que', 'on baise'], '[', ']')
+    "[Meuf] [qu'on baise]."
+    >>> assemble_sentence(['meuf que', 'Aristote démonte'], '[', ']')
+    "[Meuf] [qu'Aristote démonte]."
+    >>> assemble_sentence(['meuf que', 'Ursule encule'], '[', ']')
+    "[Meuf] [qu'Ursule encule]."
     """
-    parts = parts[:]
     result = ""
+    mb = mark_begin
+    me = mark_end
 
     def ligature(part):
         nonlocal result
         for (left, right), replace in LIGATURES.items():
-            if (result.lower().endswith(" " + left) and
+            if (result.rstrip(me).lower().endswith(" " + left) and
                     part.lower().startswith(right + " ")):
                 result = " ".join(
-                    (result[:-len(left) - 1], replace, part[len(right) + 1:]))
+                    (result.rstrip(me)[:-len(left) - 1] + me, mb + replace, part[len(right) + 1:] + me))
                 return True
-            if result.lower().endswith(" que") and part[0].lower() in 'aeiou':
-                result = result[:-1] + "'" + part
+            if result.rstrip(me).lower().endswith(" que") and part[0].lower() in "aeiou":
+                result = result.rstrip(string.whitespace + me)[:-4] + me + " " + mb + "qu'" + part + me
                 return True
         return False
 
     for i, part in enumerate(parts):
         part = part.strip()
+        if part.endswith(","):
+            part = part.rstrip(string.whitespace + ",") + ","
         if i == 0:
-            result += part[0].upper() + part[1:]
-        elif result.endswith(",") and part.startswith(","):
-            result += " " + part[1:].strip() + " "
+            result += mb + part[0].upper() + part[1:] + me
+        elif result.rstrip(me).endswith(",") and part.startswith(","):
+            result = result.rstrip(me + ",") + me + ", " + mb + part.lstrip(string.whitespace + ",") + me
         elif ligature(part):
             continue
         elif not part.startswith(","):
-            result += " " + part
+            result += " " + mb + part + me
         else:
-            result += part
+            result += ", " + mb + part.lstrip(string.whitespace + ",") + me
 
-    return result.strip() + "."
+    return result + "."
