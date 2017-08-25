625fcd87289ed768e271f938f8daa9ea9565bc7c
Mickaël Thomas <mickael9@gmail.com>
2017-08-26 00:09:54 +0200

    guard: simpler approach

--- cadavre/guard.py
+++ cadavre/guard.py
@@ -1,4 +1,3 @@
-import fnmatch
 
 from irc3.plugins.command import mask_based_policy
 
@@ -6,22 +5,9 @@ from irc3.plugins.command import mask_based_policy
 class policy(mask_based_policy):
     key = __name__ + '.masks'
 
-    def has_permission(self, mask, predicates):
+    def __call__(self, predicates, meth, client, target, args, **kwargs):
         permission = predicates.get('permission')
         if predicates.get('module') == 'irc3.plugins.command':
-            permission = 'admin'
-        if permission is None:
-            return True
-        for pattern in self.masks:
-            if fnmatch.fnmatch(mask, pattern):
-                if not isinstance(self.masks, dict):
-                    return True
-                perms = self.masks[pattern]
-                if permission in perms or 'all_permissions' in perms:
-                    return True
-        return False
-
-    def __call__(self, predicates, meth, client, target, args, **kwargs):
-        print(predicates, meth, client, target, args, kwargs)
-        if args.get('help') or self.has_permission(client, predicates):
+            permission = permission or 'admin'
+        if args.get('help') or self.has_permission(client, permission):
             return meth(client, target, args)
