f376e0520ad99ba244dca1b6eb652cb40615ccfa
Mickaël Thomas <mickael9@gmail.com>
2017-08-28 23:30:20 +0200

    Add !as to simulate a message from someone

--- cadavre/admin.py
+++ cadavre/admin.py
@@ -19,3 +19,15 @@ def reload(bot, mask, target, args):
     """
     bot.reload()
     return 'Reloaded'
+
+
+@command(name='as', permission='admin', options_first=True)
+def as_cmd(bot, mask, target, args):
+    """Simulate a message from another nick
+
+        %%as <nick> <message>...
+    """
+    nick = args['<nick>']
+    message = ' '.join(args['<message>'])
+
+    bot.dispatch(f':{nick}!{nick}@{nick} PRIVMSG {bot.nick} :{message}')
